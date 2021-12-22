3fbec7935aafcc45d1a996eeb7b7daf07189e2a4
Gianluca Benassi <benox1199@gmail.com>
2021-12-22 12:07:31 +0100

    add cast to movies/tvshows objects

--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -26,7 +26,9 @@ export default {
     data() {
         return {
             dataShared,
-            searchText: ''
+            searchText: '',
+            apiKey: 'f39e531ef2d1aa05151fd6abd9e8ca67',
+            apiLang: 'it-IT'
         }
     },
     methods: {
@@ -40,28 +42,67 @@ export default {
                 // get movies
                 axios.get('https://api.themoviedb.org/3/search/movie', {
                     params: {
-                    api_key: 'f39e531ef2d1aa05151fd6abd9e8ca67',
-                    query: this.searchText,
-                    language: 'it-IT'
+                        api_key: this.apiKey,
+                        query: this.searchText,
+                        language: this.apiLang
                     }
                 })
-                .then(function (response) {
-                    dataShared.movies = response.data.results;
+                .then((response) => {
+                    // add movies to dataShared.movies
+                    this.dataShared.movies = response.data.results;
+
+                    // get movies credits
+                    this.dataShared.movies.forEach((elm, index) => {
+                        axios.get(`https://api.themoviedb.org/3/movie/${elm.id}/credits`, {
+                            params: {
+                                api_key: this.apiKey,
+                                language: this.apiLang
+                            }
+                        })
+                        .then((response) => {
+                            // add cast property to movie object
+                            this.dataShared.movies[index].cast = (response.data.cast).slice(0,5);
+                        })
+                        .catch(function(error) {
+                            console.log(error);
+                        });
+                    });
+
                 })
                 .catch(function (error) {
                     console.log(error);
                 }); 
 
+
                 // get tv show
                 axios.get('https://api.themoviedb.org/3/search/tv', {
                     params: {
-                    api_key: 'f39e531ef2d1aa05151fd6abd9e8ca67',
-                    query: this.searchText,
-                    language: 'it-IT'
+                        api_key: this.apiKey,
+                        query: this.searchText,
+                        language: this.apiLang
                     }
                 })
-                .then(function (response) {
-                    dataShared.tvShows = response.data.results;
+                .then((response) => {
+                    // add tvShows to dataShared.tvShows
+                    this.dataShared.tvShows = response.data.results;
+
+                    // get tvShow credits
+                    this.dataShared.tvShows.forEach((elm, index) => {
+                        axios.get(`https://api.themoviedb.org/3/tv/${elm.id}/credits`, {
+                            params: {
+                                api_key: this.apiKey,
+                                language: this.apiLang
+                            }
+                        })
+                        .then((response) => {
+                            // add cast property to tvShows object
+                            this.dataShared.tvShows[index].cast = (response.data.cast).slice(0,5);
+                        })
+                        .catch(function(error) {
+                            console.log(error);
+                        });
+                    });
+
                 })
                 .catch(function (error) {
                     console.log(error);
